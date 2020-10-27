e08e34336b6bf72899f9b5d70c18702d31782345
Benkei Sándor <sanya.benkei@gmail.com>
2020-10-27 16:48:23 +0100

    vue calculator

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld/>
 </template>
 
 <script>
@@ -15,12 +14,14 @@ export default {
 </script>
 
 <style lang="scss">
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,187 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="container">
+    <div class="calculator">
+      <div class="calculator-container">
+        <div class="result">
+          <div class="show-result">{{state.current||0}}</div>
+        </div>
+        <div class="row-one">
+          <div class="seven" @click="append('7')">{{state.seven}}</div>
+          <div class="eight" @click="append('8')">{{state.eight}}</div>
+          <div class="nine" @click="append('9')">{{state.nine}}</div>
+          <div class="clear" @click="clear">C</div>
+        </div>
+      <div class="row-two">
+        <div class="four" @click="append('4')">{{state.four}}</div>
+        <div class="five" @click="append('5')">{{state.five}}</div>
+        <div class="six" @click="append('6')">{{state.six}}</div>
+        <div class="multiplication" @click="multiplication">x</div>
+      </div>
+      <div class="row-three">
+        <div class="one" @click="append('1')">{{state.one}}</div>
+        <div class="two" @click="append('2')">{{state.two}}</div>
+        <div class="three" @click="append('3')">{{state.three}}</div>
+        <div class="divide" @click="divide">/</div>
+      </div>
+      <div class="row-four">
+        <div class="zero" @click="append('0')">0</div>
+        <div class="minus" @click="minus">-</div>
+        <div class="plus" @click="plus">+</div>
+        <div class="equal" @click="equal">=</div>
+      </div>
+      </div>
   </div>
+</div>
 </template>
 
 <script>
+import {reactive} from 'vue';
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  setup(){
+    const state=reactive({
+      one:1,
+      two:2,
+      three:3,
+      four:4,
+      five:5,
+      six:6,
+      seven:7,
+      eight:8,
+      nine:9,
+
+      previous:null,
+      current:'',
+      operator:null,
+      operatorClicked:false
+    })
+    // C
+    function clear(){
+      state.current='';
+    }
+    //append numbers after the last
+    function append(number){
+      if(state.operatorClicked){
+        state.current='';
+        state.operatorClicked=false
+      }
+      state.current+=number;
+    }
+    //take current to previous
+    function setPrevious(){
+      state.previous=state.current;
+      state.operatorClicked=true
+    }
+
+    // + - / *
+    function multiplication(){
+      state.operator=(a,b)=>a*b;
+      setPrevious()
+    }
+    function plus(){
+      state.operator=(a,b)=>a+b;
+      setPrevious()
+    }
+    function minus(){
+      state.operator=(a,b)=>a-b;
+      setPrevious()
+    }
+    function divide(){
+      state.operator=(a,b)=>b/a;
+      setPrevious()
+
+    }
+    // equal and maximize decimals
+    function equal(){
+      state.current=state.operator(parseFloat(state.current),parseFloat(state.previous));
+      
+    }
+
+    return{
+      state,
+      clear,
+      multiplication,
+      append,
+      plus,
+      minus,
+      divide,
+      equal,
+      setPrevious
+    }
+    
+    
   }
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.container{
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: white;
+  .calculator{
+    width: 400px;
+    height: 600px;
+    margin: auto;
+
+    background-color: #242323;
+    border-radius: 32px;
+    box-shadow: 5px 8px 5px rgb(0, 0, 0);
+    .calculator-container{
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .row-one, .row-two, .row-three, .row-four{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 90%;
+        height: 100%;
+        div{
+          padding: 1.8rem 2rem;
+          border-radius: 16px;
+          border: 1px solid rgb(184, 184, 184);
+          font-size: 1.8rem;
+
+          cursor: pointer;
+          &:hover{
+            background-color: #fff;
+            color: #000;
+          }
+        }
+        .equal, .minus, .plus, .divide, .multiplication{
+          background-color: rgb(90, 21, 21);
+        }
+      }
+      .result{
+        height: 30%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        .show-result{
+          width: 95%;
+          padding: 1.8rem;
+          border-radius: 22px;
+          margin: .5rem auto;
+          color: #000;
+          font-size: 2rem;
+          background-color: #fff;
+          text-align: center;
+        }
+      }
+    }
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
